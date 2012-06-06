609b972ade32f6c8a1b7fd81e2be5e7f0985f076
Alexander Belozerov <belozerov.sasha@gmail.com>
2012-06-07 05:50:42 +0700

    Added temperature steps

--- main.cpp
+++ main.cpp
@@ -17,36 +17,45 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+    float T_min, T_max, T_step;
     cout << " Two-dimensional Ising Model - Swendsen-Wang Algorithm\n"
          << " -----------------------------------------------------\n"
          << " Enter number of spins L in each direction: ";
     cin >> Lx;
     Ly = Lx;
     N = Lx * Ly;
-    cout << " Enter temperature T: ";
-    cin >> T;
+    cout << " Enter min T: ";
+    cin >> T_min;
+    cout << " Enter max T: ";
+    cin >> T_max;
+    cout << " Enter T step: ";
+    cin >> T_step;
     cout << " Enter number of Monte Carlo steps: ";
     int MCSteps;
     cin >> MCSteps;
 
-    initialize();
-    initializeClusterVariables();
+    T = T_min;
+    while(T <= T_max) {
+        initialize();
+        initializeClusterVariables();
 
-    int thermSteps = MCSteps / 5;
-    cout << " Performing " << thermSteps 
-         << " thermalization steps ..." << flush;
-    for (int i = 0; i < thermSteps; i++)
-        oneMonteCarloStep();
-    cout << " done\n Performing production steps ..." << flush;
+        int thermSteps = MCSteps / 5;
+        cout << " Performing " << thermSteps 
+            << " thermalization steps ..." << flush;
+        for (int i = 0; i < thermSteps; i++)
+            oneMonteCarloStep();
+        cout << " done\n Performing production steps ..." << flush;
 
-    initializeObservables();
-    for (int i = 0; i < MCSteps; i++) {
-        oneMonteCarloStep();
-        measureObservables();
+        initializeObservables();
+        for (int i = 0; i < MCSteps; i++) {
+            oneMonteCarloStep();
+            measureObservables();
+        }
+        cout << " done" << endl;
+        computeAverages();
+        cout << "T = " << T << " | Energy per spin = " << eAve << " +- " << eError << endl;
+        T += T_step;
     }
-    cout << " done" << endl;
-    computeAverages();
-    cout << " Energy per spin = " << eAve << " +- " << eError << endl;
 }
 
 
