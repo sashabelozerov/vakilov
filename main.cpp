dc57e52edce23f8c4c7676808a087976655feec7
Sergey Zolotarev <zeex@rocketmail.com>
2012-06-07 13:21:50 +0600

    Надоело цифры вводить

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <cstdlib>
+#include <map>
+#include <sstream>
 #include <string>
 
 #include "mersenne.h"
@@ -13,33 +15,80 @@
 #include "csvwriter.h"
 using namespace std;
 
-/*
- * 
- */
-
-int main(int argc, char** argv) {
-	
-	
-    float T_min, T_max, T_step;
-    cout << " Two-dimensional Ising Model - Swendsen-Wang Algorithm\n"
-         << " -----------------------------------------------------\n"
-         << " Enter number of spins L in each direction: ";
-    cin >> Lx;
-    Ly = Lx;
-    N = Lx * Ly;
-    cout << " Enter min T: ";
-    cin >> T_min;
-    cout << " Enter max T: ";
-    cin >> T_max;
-    cout << " Enter T step: ";
-    cin >> T_step;
-    cout << " Enter number of Monte Carlo steps: ";
-    int MCSteps;
-    cin >> MCSteps;
-
+int main(int argc, char** argv) {	
+    double T_min, T_max, T_step;
+	int MCSteps;
 	std::string outFile;
-	cout << " Enter output filename: ";
-	cin >> outFile;
+
+	map<string, string> opts;
+	for (int i = 1; i < argc; i++) {
+		string arg(argv[i]);
+		string::size_type delim_pos = arg.find('=');
+		if (delim_pos != string::npos) {
+			string name(arg.begin(), arg.begin() + delim_pos);
+			string value(arg.begin() + delim_pos + 1, arg.end());
+			opts.insert(make_pair(name, value));
+		}
+	}
+
+    cout << " Two-dimensional Ising Model - Swendsen-Wang Algorithm\n"
+         << " -----------------------------------------------------\n";
+
+	if (opts["cli"] == "yes") {
+		map<string, string>::const_iterator iterator;
+		if ((iterator = opts.find("L")) != opts.end()) {
+			Lx = Ly = atoi(iterator->second.c_str());
+			N = Lx * Ly; 
+		} else {
+			cerr << "L is not set" << endl;
+			exit(1);
+		}
+		if ((iterator = opts.find("Tmin")) != opts.end()) {
+			T_min = atof(iterator->second.c_str());
+		} else {
+			cerr << "Tmin is not set" << endl;
+			exit(1);
+		}
+		if ((iterator = opts.find("Tmax")) != opts.end()) {
+			T_max = atof(iterator->second.c_str());
+		} else {
+			cerr << "Tmax is not set" << endl;
+			exit(1);
+		}
+		if ((iterator = opts.find("Tstep")) != opts.end()) {
+			T_step = atof(iterator->second.c_str());
+		} else {
+			cerr << "Tstep is not set" << endl;
+			exit(1);
+		}
+		if ((iterator = opts.find("mcs")) != opts.end()) {
+			MCSteps = atoi(iterator->second.c_str());
+		} else {
+			cerr << "mcs is not set" << endl;
+			exit(1);
+		}
+		if ((iterator = opts.find("outfile")) != opts.end()) {
+			outFile = iterator->second;
+		} else {
+			outFile = "out.txt";
+		}
+	} else {
+		cout << " Enter number of spins L in each direction: ";
+		cin >> Lx;
+		Ly = Lx;
+		N = Lx * Ly;
+		cout << " Enter min T: ";
+		cin >> T_min;
+		cout << " Enter max T: ";
+		cin >> T_max;
+		cout << " Enter T step: ";
+		cin >> T_step;
+		cout << " Enter number of Monte Carlo steps: ";
+		cin >> MCSteps;
+		cout << " Enter output filename: ";
+		cin >> outFile;
+	}
+
 	CSVWriter writer(outFile);
 	
     T = T_min;
